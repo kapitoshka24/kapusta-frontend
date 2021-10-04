a584bb484d335508dc7c9a9280691c87072bf81c
tarkravch <tarkravch@gmail.com>
2021-10-04 13:29:31 +0300

    Test commit before rebasing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import TotalBalance from './components/TotalBalance';
-import Header from './components/Header';
-import Turnover from './components/Turnover/Turnover';
-
-import TurnoverSlider from './components/Turnover-slider/TurnoverSlider';
-import TurnoverWrapper from './components/Turnover-wrapper/TurnoverWrapper';
-
+import Turnover from './components/turnover/Turnover';
 export default function App() {
   return (
     <>
-      <Header />
-      <TotalBalance />
-
       <Turnover />
-      <TurnoverWrapper>
-        <TurnoverSlider />
-      </TurnoverWrapper>
     </>
   );
 }
--- src/components/turnover/Turnover.js
+++ src/components/turnover/Turnover.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import styles from './Turnover.module.scss';
 
@@ -24,3 +25,38 @@ export default function Turnover() {
     </div>
   );
 }
+||||||| parent of 124e49d (Test commit before rebasing)
+=======
+import React from "react";
+import styles from "./Turnover.module.scss";
+
+let turnoverArr = [
+	{ id: 1, name: "Расходы", value: 18000 },
+	{ id: 2, name: "Доходы", value: 45000 }
+]
+
+
+export default function Turnover(){
+	return (
+		<>
+			<section className={styles.turnover}>
+				<ul className={styles.turnover__list}>
+				{turnoverArr.map((turnoverItem) => {
+					let adoptedValue;
+					if (turnoverItem.name === "Расходы") {
+						adoptedValue = "- "+String(turnoverItem.value.toFixed(2)) + " грн.";
+					}
+					if (turnoverItem.name === "Доходы") {
+						adoptedValue = "+ "+String(turnoverItem.value) + " грн.";
+					}
+					
+					<li key={turnoverItem.id} className={styles.turnover__item}>{turnoverItem.name} :<span className={styles.turnover_value}>{adoptedValue}</span></li>
+				})}
+	
+			</ul>
+			</section>
+			
+		</>
+	)
+}
+>>>>>>> 124e49d (Test commit before rebasing)
