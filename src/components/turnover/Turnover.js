34ea3b08b9dae499657a058ee87dffe749a94812
tarkravch <tarkravch@gmail.com>
2021-10-04 16:17:00 +0300

    [Turnover (расходы/доходы)] add HTML, SCSS

--- src/components/turnover/Turnover.js
+++ src/components/turnover/Turnover.js
@@ -1,32 +1,52 @@
-import React from "react";
-import styles from "./Turnover.module.scss";
+import React from 'react';
+import styles from './Turnover.module.scss';
 
 let turnoverArr = [
-	{ id: 1, name: "Расходы", value: 18000 },
-	{ id: 2, name: "Доходы", value: 45000 }
-]
+  { id: 1, name: 'Расходы', value: 18000 },
+  { id: 2, name: 'Доходы', value: 45000 },
+];
 
+export default function Turnover() {
+  return (
+    <>
+      <section className={styles.turnover}>
+        <ul className={styles.turnover__list}>
+          {turnoverArr.map(turnoverItem => {
+            let stringifiedValue = String(turnoverItem.value.toFixed(2));
+            let transformedValue =
+              stringifiedValue.slice(0, stringifiedValue.length - 6) +
+              stringifiedValue.slice(stringifiedValue.length - 6) +
+              ' грн.';
+            let finalValue;
 
-export default function Turnover(){
-	return (
-		<>
-			<section className={styles.turnover}>
-				<ul className={styles.turnover__list}>
-				{turnoverArr.map((turnoverItem) => {
-					let adoptedValue;
-					if (turnoverItem.name === "Расходы") {
-						adoptedValue = "- "+String(turnoverItem.value.toFixed(2)) + " грн.";
-					}
-					if (turnoverItem.name === "Доходы") {
-						adoptedValue = "+ "+String(turnoverItem.value) + " грн.";
-					}
-					
-					<li key={turnoverItem.id} className={styles.turnover__item}>{turnoverItem.name} :<span className={styles.turnover_value}>{adoptedValue}</span></li>
-				})}
-	
-			</ul>
-			</section>
-			
-		</>
-	)
+            if (turnoverItem.name === 'Доходы') {
+              finalValue = '+ ' + transformedValue;
+            }
+            if (turnoverItem.name === 'Расходы') {
+              finalValue = '- ' + transformedValue;
+            }
+            return (
+              <li key={turnoverItem.id} className={styles.turnover__item}>
+                {turnoverItem.name} :
+                {turnoverItem.name === 'Расходы' && (
+                  <span
+                    className={`${styles.turnover_value} ${styles.expense_color}`}
+                  >
+                    {finalValue}
+                  </span>
+                )}
+                {turnoverItem.name === 'Доходы' && (
+                  <span
+                    className={`${styles.turnover_value} ${styles.income_color}`}
+                  >
+                    {finalValue}
+                  </span>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
+  );
 }
