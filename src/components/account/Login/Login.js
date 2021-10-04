fc42c165f38ea3670d58a1b170959ac0c13484d9
Urko Movchan <xyyrma@gmail.com>
2021-10-04 16:12:08 +0300

    add formik, sass module

--- src/components/account/Login/Login.js
+++ src/components/account/Login/Login.js
@@ -1,53 +1,105 @@
+import { useFormik } from 'formik';
+
 import googleSymbol from '../../../images/google-symbol.svg';
+import styles from './login.module.scss';
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = 'это обязательное поле';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'не правильный email';
+  }
+
+  if (!values.password) {
+    errors.password = 'это обязательное поле';
+  }
+
+  return errors;
+};
 
 export default function Login() {
+  const { errors, values, handleSubmit, handleChange } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div className="login">
-      <div className="login__body">
-        <p className="login__title login__title--google">
+    <div className={styles.login}>
+      <div className={styles.loginBody}>
+        <p className={{ ...styles.loginTitle, ...styles.loginTitleGoogle }}>
           Вы можете авторизоваться с помощью Google Account:
         </p>
-        <button className="login__google" onClick={() => {}}>
+        <button className={styles.loginGoogle} onClick={() => {}}>
           <img
             src={googleSymbol}
             alt="Google Symbol"
-            className="login__googleSymbol"
+            className={styles.loginGoogleSymbol}
           />
           Google
         </button>
-        <p className="login__title">
+        <p className={styles.loginTitle}>
           Или зайти с помощью e-mail и пароля, предварительно
           зарегистрировавшись:
         </p>
-        <div className="login__group">
-          <label className="login__label" htmlFor="inputEmail">
-            Электронная почта:
-          </label>
-          <input
-            type="email"
-            id="inputEmail"
-            className="login__input"
-            placeholder="your@email.com"
-          />
-        </div>
-
-        <div className="login__group">
-          <label className="login__label" htmlFor="inputPassword">
-            Пароль:
-          </label>
-          <input
-            type="password"
-            id="inputPassword"
-            className="login__input"
-            placeholder="Пароль"
-          />
-        </div>
       </div>
 
-      <div className="login__buttons">
-        <button>Войти</button>
-        <button>Регистрация</button>
-      </div>
+      <form onSubmit={handleSubmit} noValidate>
+        <div className={styles.loginBody}>
+          <div className={styles.loginGroup}>
+            <label className={styles.loginLabel} htmlFor="inputEmail">
+              {errors.email ? (
+                <span className={styles.errorStar}>*</span>
+              ) : null}
+              Электронная почта:
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="inputEmail"
+              className={styles.loginInput}
+              placeholder="your@email.com"
+              onChange={handleChange}
+              value={values.email}
+            />
+            {errors.email ? (
+              <div className={styles.error}>{errors.email}</div>
+            ) : null}
+          </div>
+
+          <div className={styles.loginGroup}>
+            <label className={styles.loginLabel} htmlFor="inputPassword">
+              {errors.password ? (
+                <span className={styles.errorStar}>*</span>
+              ) : null}
+              Пароль:
+            </label>
+            <input
+              type="password"
+              id="inputPassword"
+              name="password"
+              className={styles.loginInput}
+              placeholder="Пароль"
+              onChange={handleChange}
+              value={values.password}
+            />
+            {errors.password ? (
+              <div className={styles.error}>{errors.password}</div>
+            ) : null}
+          </div>
+        </div>
+        <div className={styles.loginButtons}>
+          <button>Войти</button>
+          <button type="button">Регистрация</button>
+        </div>
+      </form>
     </div>
   );
 }
--- src/components/account/Register/Register.js
+++ src/components/account/Register/Register.js
@@ -1,62 +1,143 @@
+import { useFormik } from 'formik';
+import styles from './register.module.scss';
+const validate = values => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = 'это обязательное поле';
+  }
+
+  if (!values.email) {
+    errors.email = 'это обязательное поле';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'не правильный email';
+  }
+
+  if (!values.password) {
+    errors.password = 'это обязательное поле';
+  } else if (values.password.length < 6) {
+    errors.password = 'пароль должен быть не меньше 6 символов';
+  }
+
+  if (!values.confirm) {
+    errors.confirm = 'это обязательное поле';
+  } else if (values.password !== values.confirm) {
+    errors.confirm = 'пароли не совпадают';
+  }
+
+  return errors;
+};
+
 export default function Register() {
+  const { errors, values, handleSubmit, handleChange } = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirm: '',
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div className="register">
-      <div className="register__body">
-        <p className="register__title register__title--google">Регистрация</p>
-
-        <div className="register__group">
-          <label className="register__label" htmlFor="inputName">
-            Имя:
-          </label>
-          <input
-            type="text"
-            id="inputName"
-            className="register__input"
-            placeholder="Имя"
-          />
-        </div>
-
-        <div className="register__group">
-          <label className="register__label" htmlFor="inputEmail">
-            Электронная почта:
-          </label>
-          <input
-            type="email"
-            id="inputEmail"
-            className="register__input"
-            placeholder="your@email.com"
-          />
-        </div>
-
-        <div className="register__group">
-          <label className="register__label" htmlFor="inputPassword">
-            Пароль:
-          </label>
-          <input
-            type="password"
-            id="inputPassword"
-            className="register__input"
-            placeholder="Пароль"
-          />
-        </div>
-
-        <div className="register__group">
-          <label className="register__label" htmlFor="inputConfirm">
-            Подтверждение пароля:
-          </label>
-          <input
-            type="password"
-            id="inputConfirm"
-            className="register__input"
-            placeholder="Подтвердите пароль"
-          />
-        </div>
+    <div className={styles.register}>
+      <div className={styles.registerBody}>
+        <p className={styles.registerTitleRegister}>Регистрация</p>
       </div>
 
-      <div className="register__buttons">
-        <button>Войти</button>
-        <button>Регистрация</button>
-      </div>
+      <form onSubmit={handleSubmit} noValidate>
+        <div className={styles.registerBody}>
+          <div className={styles.registerGroup}>
+            <label className={styles.registerLabel} htmlFor="inputName">
+              {errors.name ? <span className={styles.errorStar}>*</span> : null}
+              Имя:
+            </label>
+            <input
+              type="text"
+              id="inputName"
+              name="name"
+              className={styles.registerInput}
+              placeholder="Имя"
+              onChange={handleChange}
+              value={values.name}
+            />
+            {errors.name ? (
+              <div className={styles.error}>{errors.name}</div>
+            ) : null}
+          </div>
+
+          <div className={styles.registerGroup}>
+            <label className={styles.registerLabel} htmlFor="inputEmail">
+              {errors.email ? (
+                <span className={styles.errorStar}>*</span>
+              ) : null}
+              Электронная почта:
+            </label>
+            <input
+              type="email"
+              id="inputEmail"
+              name="email"
+              className={styles.registerInput}
+              placeholder="your@email.com"
+              onChange={handleChange}
+              value={values.email}
+            />
+            {errors.email ? (
+              <div className={styles.error}>{errors.email}</div>
+            ) : null}
+          </div>
+
+          <div className={styles.registerGroup}>
+            <label className={styles.registerLabel} htmlFor="inputPassword">
+              {errors.password ? (
+                <span className={styles.errorStar}>*</span>
+              ) : null}
+              Пароль:
+            </label>
+            <input
+              type="password"
+              id="inputPassword"
+              name="password"
+              className={styles.registerInput}
+              placeholder="Пароль"
+              onChange={handleChange}
+              value={values.password}
+            />
+            {errors.password ? (
+              <div className={styles.error}>{errors.password}</div>
+            ) : null}
+          </div>
+
+          <div className={styles.registerGroup}>
+            <label className={styles.registerLabel} htmlFor="inputConfirm">
+              {errors.confirm ? (
+                <span className={styles.errorStar}>*</span>
+              ) : null}
+              Подтверждение пароля:
+            </label>
+            <input
+              type="password"
+              id="inputConfirm"
+              name="confirm"
+              className={styles.registerInput}
+              placeholder="Подтвердите пароль"
+              onChange={handleChange}
+              value={values.confirm}
+            />
+            {errors.confirm ? (
+              <div className={styles.error}>{errors.confirm}</div>
+            ) : null}
+          </div>
+        </div>
+
+        <div className={styles.registerButtons}>
+          <button>Войти</button>
+          <button>Регистрация</button>
+        </div>
+      </form>
     </div>
   );
 }
