c10e3bd94dd7b1137d450319aa01e8132bf550ce
tarkravch <tarkravch@gmail.com>
2021-10-04 13:29:31 +0300

    Test commit before rebasing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TotalBalance from './components/TotalBalance';
 import Header from './components/Header';
+import Turnover from './components/turnover/Turnover';
 
 export default function App() {
   return (
     <>
       <Header />
       <TotalBalance />
+      <Turnover />
     </>
   );
 }
