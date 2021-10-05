723bceadac7a316471db9822308c8a7f7d9fdc2b
AndrewMozharovsky <kainnet.job@gmail.com>
2021-10-05 21:39:35 +0300

    [modal] add styles

--- src/components/LogoutModal/LogoutModal.js
+++ src/components/LogoutModal/LogoutModal.js
@@ -1,12 +1,20 @@
 import styles from './LogoutModal.module.scss';
+import closeButton from '../../images/close.svg';
 
 const LogoutModal = ({ massage }) => {
   return (
     <div className={styles.backdrop}>
       <div className={styles.modalContainer}>
         <p className={styles.massage}>{massage}</p>
-        <button type="button">Да</button>
-        <button type="button">Нет</button>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button} type="button">
+            Да
+          </button>
+          <button className={styles.button} type="button">
+            Нет
+          </button>
+        </div>
+        <button className={styles.closeButton} type="button"></button>
       </div>
     </div>
   );
