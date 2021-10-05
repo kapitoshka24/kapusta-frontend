bbd238b7cdf788e710b76eb968d28b9fc493de97
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-10-05 12:10:41 +0300

    Add styles to header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 export default function App() {
   return (
     <>
+      <Header />
       <h1>replace to component</h1>
     </>
   );
