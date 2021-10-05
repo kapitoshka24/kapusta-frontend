55e25ef63ecbd070dae888b7ee860b8135a1509b
tarkravch <tarkravch@gmail.com>
2021-10-05 19:46:21 +0300

    [Expenses] Add HTML/SCSS with problems

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Turnover from './components/turnover/Turnover';
+import Expenses from './components/expenses/Expenses';
+import TurnoverSlider from './components/turnover-slider/TurnoverSlider';
+
 export default function App() {
   return (
     <>
-      <Turnover/>
+      <Turnover />
+      <TurnoverSlider />
+      <Expenses />
     </>
   );
 }
