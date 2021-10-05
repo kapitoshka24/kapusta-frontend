1da61f30f69dcc9222582870dd37aa699317d003
tarkravch <tarkravch@gmail.com>
2021-10-05 19:46:21 +0300

    [Expenses] Add HTML/SCSS with problems

--- src/components/turnover/Turnover.js
+++ src/components/turnover/Turnover.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './Turnover.module.scss';
 
 let turnoverArr = [
-  { id: 1, name: 'Расходы', value: 18000 },
-  { id: 2, name: 'Доходы', value: 45000 },
+  { id: 1, name: 'Расходы', value: 23 },
+  { id: 2, name: 'Доходы', value: 300 },
 ];
 
 export default function Turnover() {
@@ -13,10 +13,16 @@ export default function Turnover() {
         <ul className={styles.turnover__list}>
           {turnoverArr.map(turnoverItem => {
             let stringifiedValue = String(turnoverItem.value.toFixed(2));
-            let transformedValue =
-              stringifiedValue.slice(0, stringifiedValue.length - 6) +
-              stringifiedValue.slice(stringifiedValue.length - 6) +
-              ' грн.';
+            let transformedValue;
+            if (stringifiedValue.length >= 7) {
+              transformedValue =
+                stringifiedValue.slice(0, stringifiedValue.length - 6) +
+                ' ' +
+                stringifiedValue.slice(stringifiedValue.length - 6) +
+                ' грн.';
+            } else {
+              transformedValue = stringifiedValue + ' грн.';
+            }
 
             return (
               <li key={turnoverItem.id} className={styles.turnover__item}>
