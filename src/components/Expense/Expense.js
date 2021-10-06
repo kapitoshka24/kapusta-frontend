e1e51b8ffe5f40ac5697787bdc4f4ae5a779dd07
AndrewMozharovsky <kainnet.job@gmail.com>
2021-10-07 01:10:50 +0300

    [income-modal] add styles

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -2,41 +2,58 @@ import { useState } from 'react';
 
 import TabContainer from '../TabContainer';
 import Controls from '../Controls';
+import ControlsMobile from '../ControlsMobile';
 import Tabs from '../Tabs';
 import TabsMobile from '../TabsMobile';
 import Date from '../Date';
 import EnterButton from '../EnterButton';
 import Table from '../Table';
 import TableMobile from '../TableMobile';
-// import ControlsMobile from '../ControlsMobile';
+import MonthlySummary from '../MonthlySummary';
 import styles from './Expense.module.scss';
+import useWindowWidth from '../../helpers/useWindowWidth';
 
 export default function Expense() {
   const [showControls, setShowControls] = useState(false);
+  const [showButton, setShowButton] = useState(false);
 
   const toggleControls = () => {
     setShowControls(prevVal => !prevVal);
   };
 
-  const mobile = window.screen.width < 768;
+  const showEnterButton = () => {
+    setShowButton(true);
+  };
+
+  const windowWidth = useWindowWidth();
 
   return (
     <>
-      {mobile ? <TabsMobile /> : <Tabs />}
+      {windowWidth < 768 ? (
+        <TabsMobile showButton={showEnterButton} />
+      ) : (
+        <Tabs />
+      )}
 
       <TabContainer>
-        {mobile && (
-          <div className={styles.date__mobile_container}>
-            <Date />
+        <div className={styles.date__mobile_container}>
+          {windowWidth < 768 && <Date />}
+          {windowWidth < 768 && showButton && (
             <EnterButton closeControls={toggleControls} />
-          </div>
+          )}
+        </div>
+
+        {windowWidth < 768 ? null : <Controls />}
+        {windowWidth < 768 && showControls && (
+          <ControlsMobile closeControls={toggleControls} />
         )}
 
-        {mobile ? null : <Controls />}
+        <div className={styles.data__container}>
+          {windowWidth < 768 ? null : <Table />}
+          {windowWidth < 768 && showButton && <TableMobile />}
 
-        {showControls && <Controls closeControls={toggleControls} />}
-
-        {mobile ? <TableMobile /> : <Table />}
+          <MonthlySummary />
+        </div>
       </TabContainer>
     </>
   );
