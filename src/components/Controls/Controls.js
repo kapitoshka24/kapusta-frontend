d951603b5fb7a6b188c55dbf3b10629034cc2ed5
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-10-06 22:31:06 +0300

    [Expense] Finish tablet css

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,28 +1,32 @@
 import Select from '../Select';
 
+import Date from '../Date';
 import styles from './Controls.module.scss';
-import { ReactComponent as Calendar } from '../../icons/calendar.svg';
 import { ReactComponent as Calculator } from '../../icons/calculator.svg';
 
 export default function Controls() {
   return (
     <div className={styles.controls__container}>
-      <div className={styles.inputs__container}>
-        <Calendar className={styles.icon__calendar} />
+      <div className={styles.inputs__date__thumb}>
+        <Date />
 
-        <span className={styles.date}>21.11.2019</span>
+        <div className={styles.inputs__container}>
+          <input
+            type="text"
+            placeholder="Описание товара"
+            className={styles.input__item}
+          />
 
-        <input
-          type="text"
-          placeholder="Описание товара"
-          className={styles.input__item}
-        />
+          <Select />
 
-        <Select />
-
-        <div className={styles.input__sum__thumb}>
-          <input type="text" placeholder="0,00" className={styles.input__sum} />
-          <Calculator className={styles.icon__calculator} />
+          <div className={styles.input__sum__thumb}>
+            <input
+              type="text"
+              placeholder="0,00"
+              className={styles.input__sum}
+            />
+            <Calculator className={styles.icon__calculator} />
+          </div>
         </div>
       </div>
 
