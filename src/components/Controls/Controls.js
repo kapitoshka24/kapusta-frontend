e6f4c95ee7a217e84f3baf848cd3520ca1c559de
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-10-06 22:31:06 +0300

    [Expense] Finish tablet css

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,31 +1,26 @@
 import Select from '../Select';
 
-<<<<<<< HEAD
 import Date from '../Date';
+// import BackToMainPage from '../report/BackToMainPage';
 import styles from './Controls.module.scss';
 import { ReactComponent as Calculator } from '../../icons/calculator.svg';
-
-export default function Controls({ closeControls }) {
-  const mobile = window.screen.width < 768;
-
-||||||| parent of f902ba7 ([Expense] Add dropdown & tabs components)
-export default function Controls() {
-=======
-import styles from './Controls.module.scss';
-import { ReactComponent as Calendar } from '../../icons/calendar.svg';
-import { ReactComponent as Calculator } from '../../icons/calculator.svg';
+import { ReactComponent as GoBack } from '../../icons/go-home.svg';
+import useWindowWidth from '../../helpers/useWindowWidth';
 
 export default function Controls() {
->>>>>>> f902ba7 ([Expense] Add dropdown & tabs components)
+  const windowWidth = useWindowWidth();
+
   return (
-<<<<<<< HEAD
     <div className={styles.controls__container}>
-      <div className={styles.inputs__date__thumb}>
-        {!mobile && <Date />}
+      <div className={styles.date__container}>
+        {windowWidth >= 768 && <Date />}
+      </div>
 
-        <button className={styles.arrow} onClick={closeControls} type="button">
-          Стрелка
-        </button>
+      <button className={styles.arrow} type="button">
+        <GoBack />
+      </button>
+      {/* <BackToMainPage onClick={closeControls} /> */}
+      <form className={styles.form}>
         <div className={styles.inputs__container}>
           <input
             type="text"
@@ -47,47 +42,16 @@ export default function Controls() {
             </div>
           </div>
         </div>
-      </div>
-||||||| parent of f902ba7 ([Expense] Add dropdown & tabs components)
-    <div className={styles.input__container}>
-      <span className={styles.date}>21.11.2019</span>
-      <input
-        type="text"
-        placeholder="Описание товара"
-        className={styles.input__item}
-      />
-      <Select />
-      <input type="text" placeholder="0,00" className={styles.input__sum} />
-=======
-    <div className={styles.controls__container}>
-      <div className={styles.inputs__container}>
-        <Calendar className={styles.icon__calendar} />
 
-        <span className={styles.date}>21.11.2019</span>
-
-        <input
-          type="text"
-          placeholder="Описание товара"
-          className={styles.input__item}
-        />
-
-        <Select />
-
-        <div className={styles.input__sum__thumb}>
-          <input type="text" placeholder="0,00" className={styles.input__sum} />
-          <Calculator className={styles.icon__calculator} />
+        <div className={styles.buttons__container}>
+          <button type="submit" className={styles.button__submit}>
+            Ввод
+          </button>
+          <button type="reset" className={styles.button__reset}>
+            Очистить
+          </button>
         </div>
-      </div>
->>>>>>> f902ba7 ([Expense] Add dropdown & tabs components)
-
-      <div className={styles.buttons__container}>
-        <button type="submit" className={styles.button__submit}>
-          Ввод
-        </button>
-        <button type="reset" className={styles.button__reset}>
-          Очистить
-        </button>
-      </div>
+      </form>
     </div>
   );
 }
