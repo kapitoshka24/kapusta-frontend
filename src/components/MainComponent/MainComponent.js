add0cde7f6f11dc39d02e13c5582805eb9f787e4
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-10-08 23:27:08 +0300

    Fix: background

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,8 +1,6 @@
 import { ReactComponent as Hero } from './icons/Union.svg';
 import { ReactComponent as HeroTablet } from './icons/Union-tablet.svg';
 import { ReactComponent as HeroDesktop } from './icons/Union-desktop.svg';
-import { ReactComponent as Cabbage } from './icons/Vector.svg';
-import { ReactComponent as Shadow } from './icons/Ellipse.svg';
 
 import styles from './MainComponent.module.scss';
 
@@ -11,7 +9,11 @@ export default function MainComponent() {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.hero__container}>
+      <div
+        className={`${styles.hero__container} ${
+          isLogin ? styles.hero__login : ''
+        }`}
+      >
         {!isLogin && (
           <>
             <div className={styles.hero}>
@@ -27,112 +29,9 @@ export default function MainComponent() {
           </>
         )}
       </div>
-      <div className={styles.footer}>
-        {!isLogin && (
-          <>
-            <div className={styles.cabbage1}>
-              <Cabbage />
-            </div>
-            <div className={styles.cabbage2}>
-              <Cabbage />
-            </div>
-            <div className={styles.cabbages__tablet}>
-              <div className={styles.cabbage3}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage4}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage5}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage6}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage7}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage8}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage9}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage10}>
-                <Cabbage />
-              </div>
-              <div className={styles.shadow1}>
-                <Shadow />
-              </div>
-              <div className={styles.shadow2}>
-                <Shadow />
-              </div>
-              <div className={styles.cabbages__desktop}>
-                <div className={styles.cabbage15}>
-                  <Cabbage />
-                </div>
-                <div className={styles.cabbage16}>
-                  <Cabbage />
-                </div>
-                <div className={styles.cabbage13}>
-                  <Cabbage />
-                </div>
-                <div className={styles.cabbage14}>
-                  <Cabbage />
-                </div>
-              </div>
-            </div>
-          </>
-        )}
-        {isLogin && (
-          <>
-            <div className={styles.cabbage11}>
-              <Cabbage />
-            </div>
-            <div className={styles.cabbage12}>
-              <Cabbage />
-            </div>
-            <div className={styles.shadow3}>
-              <Shadow />
-            </div>
-            <div className={styles.shadow4}>
-              <Shadow />
-            </div>
-            <div className={styles.cabbages__desktop__isLogin}>
-              <div className={styles.cabbage17}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage18}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage19}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage20}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage21}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage22}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage23}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage24}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage25}>
-                <Cabbage />
-              </div>
-              <div className={styles.cabbage26}>
-                <Cabbage />
-              </div>
-            </div>
-          </>
-        )}
-      </div>
+      <div
+        className={`${styles.footer} ${isLogin ? styles.footer__login : ''}`}
+      ></div>
     </div>
   );
 }
