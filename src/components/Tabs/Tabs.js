16fa1ba28b38ea4a489b00846e22cda45a0da7f4
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-10-08 17:22:57 +0300

    [Expense/Income] Add logic for mobile components

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,15 +1,38 @@
+import { useState } from 'react';
+
 import styles from './Tabs.module.scss';
 
 export default function Tabs() {
+  const [isActive, setActive] = useState([true, false]);
+
+  const toggleClass = e => {
+    e.target.id === '0' ? setActive([true, false]) : setActive([false, true]);
+  };
+
   return (
     <div className={styles.button__container}>
       <button
         type="button"
-        className={[styles.button, styles.button__active].join(' ')}
+        id="0"
+        className={
+          isActive[0]
+            ? [styles.button, styles.button__active].join(' ')
+            : styles.button
+        }
+        onClick={toggleClass}
       >
         Расход
       </button>
-      <button type="button" className={styles.button}>
+      <button
+        type="button"
+        id="1"
+        className={
+          isActive[1]
+            ? [styles.button, styles.button__active].join(' ')
+            : styles.button
+        }
+        onClick={toggleClass}
+      >
         Доход
       </button>
     </div>
--- src/components/TabsMobile/TabsMobile.js
+++ src/components/TabsMobile/TabsMobile.js
@@ -1,15 +1,44 @@
+import { useState } from 'react';
+
 import styles from '../Tabs/Tabs.module.scss';
 
-export default function TabsMobile() {
+export default function TabsMobile({ showButton }) {
+  const [isActive, setActive] = useState([false, false]);
+
+  const toggleClass = e => {
+    e.target.id === '0' ? setActive([true, false]) : setActive([false, true]);
+  };
+
   return (
     <div className={styles.button__container}>
       <button
+        id="0"
         type="button"
-        className={[styles.button, styles.button__active].join(' ')}
+        className={
+          isActive[0]
+            ? [styles.button, styles.button__active].join(' ')
+            : styles.button
+        }
+        onClick={e => {
+          showButton();
+          toggleClass(e);
+        }}
       >
         Расход
       </button>
-      <button type="button" className={styles.button}>
+      <button
+        id="1"
+        type="button"
+        className={
+          isActive[1]
+            ? [styles.button, styles.button__active].join(' ')
+            : styles.button
+        }
+        onClick={e => {
+          showButton();
+          toggleClass(e);
+        }}
+      >
         Доход
       </button>
     </div>
