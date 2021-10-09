f425600afa849c4e92864b02e01aeca3b517129f
Nataliia Byshovets <natashabysh11@gmail.com>
2021-10-09 23:26:14 +0300

    Minor style fixes

--- src/components/BackToMainPage/BackToMainPage.js
+++ src/components/BackToMainPage/BackToMainPage.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './BackToMainPage.module.scss';
 import { ReactComponent as GoHomeIcon } from '../../icons/go-home.svg';
-import useWindowWidth from '../../helpers/useWindowWidth';
 
 function BackToMainPage({ closeModal }) {
-  const windowWidth = useWindowWidth();
-
   return (
     <Link to="/home" className={styles.Link} onClick={closeModal}>
       <GoHomeIcon width="18" height="12" className={styles.GoHomeIcon} />
-      {windowWidth >= 768 && (
-        <span className={styles.TextLink}>Вернуться на главную</span>
-      )}
+      <span className={styles.TextLink}>Вернуться на главную</span>
     </Link>
   );
 }
