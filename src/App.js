e483c6c385c2a1f515e9fee7506b38660ea197c1
Nataliia Byshovets <natashabysh11@gmail.com>
2021-10-10 17:36:12 +0300

    minor fix

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 // import Header from './components/Header';
 // import LoginPage from './pages/LoginPage';
 // import RegisterPage from './pages/RegisterPage';
-import ExpenceIncomePage from './pages/ExpenseIncomePage';
-// import ReportPage from './pages/ReportPage';
+// import ExpenceIncomePage from './pages/ExpenseIncomePage';
+import ReportPage from './pages/ReportPage';
 // import Expense from './components/ExpenseIncome/Expense';
 
 export default function App() {
