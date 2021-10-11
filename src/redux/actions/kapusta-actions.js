0772615b6b9030348039ecc194f307f6562b18c6
Elena Vlasenko <olefelena@gmail.com>
2021-10-11 20:02:12 +0300

    bugfix/redux-actions kapusta

--- src/redux/actions/kapusta-actions.js
+++ src/redux/actions/kapusta-actions.js
@@ -1,6 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const changeTotalBalance = createAction('ckapusta/changeTotalBalance');
+export const changeTotalBalance = createAction('kapusta/changeTotalBalance');
 
 export const fetchMonthlySummaryRequest = createAction('kapusta/fetchMonthlySummaryRequest');
 export const fetchMonthlySummarySuccess = createAction('kapusta/fetchMonthlySummarySuccess');
