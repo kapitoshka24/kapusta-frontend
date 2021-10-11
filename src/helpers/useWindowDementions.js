6b74267489da8bda506d3095e4c14755713f74e6
Nataliia Byshovets <natashabysh11@gmail.com>
2021-10-11 17:54:14 +0300

    delete comments

--- src/helpers/useWindowDementions.js
+++ src/helpers/useWindowDementions.js
@@ -1,20 +1,5 @@
 import { useState, useEffect } from 'react';
 
-// export default function useWindowWidth() {
-//   const [windowWidth, setWindowWidth] = useState(window.screen.width);
-
-//   useEffect(() => {
-//     function handleResize() {
-//       setWindowWidth(window.screen.width);
-//     }
-
-//     window.addEventListener('resize', handleResize);
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []);
-
-//   return windowWidth;
-// }
-
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return {
