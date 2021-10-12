b03ae6cbb184a006cf9b59d4144e0de3b74de6c8
Elena Vlasenko <olefelena@gmail.com>
2021-10-12 11:14:01 +0300

    [MonthlySummary]/added logic implementation

--- src/components/ExpenseIncome/MonthlySummary/MonthlySummary.js
+++ src/components/ExpenseIncome/MonthlySummary/MonthlySummary.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useCallback }  from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './MonthlySummary.module.scss';
+import {getMonthlySummary} from '../../../redux/selectors/kapusta-selectors'
+import {fetchMonthlySummary} from '../../../redux/operations/kapusta-operations'
 
 function MonthlySummary() {
+  const dispatch = useDispatch();
+  const list = useSelector(getMonthlySummary);
+
+  const onFetchMonthlySummary = useCallback(
+    () => dispatch(fetchMonthlySummary()),
+    [dispatch],
+  );
+  useEffect(() => onFetchMonthlySummary(), [onFetchMonthlySummary]);
+
   return (
     <div className={styles.SummaryWidget}>
       <h2 className={styles.SummaryTitle}>Сводка</h2>
       <ul className={styles.MonthList}>
-        <li className={styles.MonthItem}>
+        {list.map(({_id,total})=> (
+          <li key={_id} className={styles.MonthItem}>
+            <span>{_id}</span>
+          <span>{total}</span>
+          </li>
+        )) }
+        {/* <li className={styles.MonthItem}>
           <span>НОЯБРЬ</span>
           <span>25 500.00</span>
         </li>
@@ -29,7 +47,7 @@ function MonthlySummary() {
         <li className={styles.MonthItem}>
           <span>ИЮНЬ</span>
           <span>18 000.00</span>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
--- src/redux/operations/kapusta-operations.js
+++ src/redux/operations/kapusta-operations.js
@@ -5,12 +5,14 @@ import {
     fetchMonthlySummarySuccess,
     fetchMonthlySummaryError } from '../actions';
 
+    axios.defaults.baseURL = 'https://kapusta-backend.herokuapp.com/api';
+
 export const fetchMonthlySummary = () => async dispatch => {
     dispatch(fetchMonthlySummaryRequest());
     
     try {
-        const response = await axios.get('https://kapusta-backend.herokuapp.com/api/currencymovements/summaryIncome?year=2021');
-        dispatch(fetchMonthlySummarySuccess(response.data));
+        const response = await axios.get('/currencymovements/summaryIncome?year=2021');
+        dispatch(fetchMonthlySummarySuccess(response.data.result));
     } catch (error) {
         dispatch(fetchMonthlySummaryError(error));
     }
