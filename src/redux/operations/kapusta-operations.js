1c75ed23662eae33a6369fabf13eceb4982192a5
Elena Vlasenko <olefelena@gmail.com>
2021-10-13 10:25:01 +0300

    implement logic realization

--- src/redux/operations/kapusta-operations.js
+++ src/redux/operations/kapusta-operations.js
@@ -3,18 +3,50 @@ import { kapustaActions } from '../actions';
 
 axios.defaults.baseURL = 'https://kapusta-backend.herokuapp.com/api';
 
-const fetchMonthlySummary = () => async dispatch => {
-  dispatch(kapustaActions.fetchMonthlySummaryRequest());
-
+const fetchTotalBalance = () => async dispatch => {
+  dispatch(kapustaActions.totalBalanceRequest);
   try {
-    const response = await axios.get(
-      '/currencymovements/summaryIncome?year=2021',
-    );
-    dispatch(kapustaActions.fetchMonthlySummarySuccess(response.data.result));
+    const { data } = await axios.get('/currencymovements/balance');
+
+    dispatch(kapustaActions.totalBalanceSuccess(data));
   } catch (error) {
-    dispatch(kapustaActions.fetchMonthlySummaryError(error));
+    dispatch(kapustaActions.totalBalanceError(error));
   }
 };
 
-const operations = { fetchMonthlySummary };
+const addTotalBalance = balance => async dispatch => {
+  const balanceObj = {
+    date: Date.now(),
+    name: 'adjustment',
+    category: 'adjustments',
+    sum: balance,
+  };
+
+  dispatch(kapustaActions.addTotalBalanceRequest);
+  try {
+    await axios.post('/currencymovements/create', balanceObj);
+    dispatch(kapustaActions.addTotalBalanceSuccess(balance));
+  } catch (error) {
+    dispatch(kapustaActions.addTotalBalanceError(error));
+  }
+};
+
+const fetchMonthlySummary = () => async dispatch => {
+    dispatch(kapustaActions.fetchMonthlySummaryRequest());
+    
+    try {
+        const response = await axios.get('/currencymovements/summaryExpenses?year=2021');
+        dispatch(kapustaActions.fetchMonthlySummarySuccess(response.data.result));
+    } catch (error) {
+        dispatch(kapustaActions.fetchMonthlySummaryError(error));
+    }
+};
+
+
+const operations = {
+  fetchTotalBalance,
+  addTotalBalance,
+  fetchMonthlySummary
+};
+
 export default operations;
--- src/redux/reducers/kapusta-reducer.js
+++ src/redux/reducers/kapusta-reducer.js
@@ -3,8 +3,10 @@ import { createReducer } from '@reduxjs/toolkit';
 
 import { kapustaActions } from '../actions';
 
-const totalBalance = createReducer(0, {
-  [kapustaActions.changeTotalBalance]: (_, { payload }) => payload,
+const totalBalance = createReducer('', {
+  [kapustaActions.totalBalanceSuccess]: (_, { payload }) =>
+    payload.data.balance,
+  [kapustaActions.addTotalBalanceSuccess]: (_, { payload }) => payload,
 });
 
 const monthlySummary = createReducer([], {
@@ -12,12 +14,9 @@ const monthlySummary = createReducer([], {
 });
 
 const error = createReducer(null, {
-  [kapustaActions.fetchMonthlySummaryError]: (_, { payload }) =>
-    console.log(payload),
+  [kapustaActions.fetchMonthlySummaryError]: (_, { payload }) => console.log(payload),
 });
 
 export default combineReducers({
-  totalBalance,
-  monthlySummary,
-  error,
+  totalBalance, monthlySummary, error
 });
