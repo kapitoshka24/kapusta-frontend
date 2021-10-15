4a8fe3549ba6c2ac9312d9f4d4b88dc05794da6c
Elena Vlasenko <olefelena@gmail.com>
2021-10-15 16:30:22 +0300

    [MonthlySummary]/changed api route

--- src/redux/operations/kapusta-operations.js
+++ src/redux/operations/kapusta-operations.js
@@ -36,7 +36,7 @@ const fetchMonthlySummary = () => async dispatch => {
 
   try {
     const response = await axios.get(
-      '/currencymovements/summaryExpenses?year=2021',
+      '/currency-movements/summary-expenses?year=2021',
     );
     dispatch(kapustaActions.fetchMonthlySummarySuccess(response.data.result));
   } catch (error) {
