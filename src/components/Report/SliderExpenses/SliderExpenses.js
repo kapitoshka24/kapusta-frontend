77de49c5cc9137aec25efce2f80890c494616ce0
Fomenko Vladyslav <fomenkovlad93@gmail.com>
2021-10-17 02:37:56 +0300

    [Report] add temporary slider

--- src/components/Report/SliderExpenses/SliderExpenses.js
+++ src/components/Report/SliderExpenses/SliderExpenses.js
@@ -2,14 +2,32 @@ import { useSelector } from 'react-redux';
 import { kapustaSelectors } from '../../../redux/selectors';
 import SliderExpensesList from './SliderExpensesList';
 import SliderNotification from '../SliderNotification';
+// import Loader from 'react-loader-spinner';
 
 const SliderExpenses = () => {
   const expenses = useSelector(kapustaSelectors.getCategoryExpenses);
 
-  return expenses && expenses.length > 0 ? (
-    <SliderExpensesList expenses={expenses} />
-  ) : (
-    <SliderNotification category="расходов" />
+  return (
+    <>
+      {expenses && expenses.length > 0 ? (
+        <SliderExpensesList expenses={expenses} />
+      ) : (
+        <SliderNotification category="расходов" />
+      )}
+
+      {/* {true && (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <Loader type="ThreeDots" color="#FF751D" height={50} width={50} />
+        </div>
+      )} */}
+    </>
   );
 };
 
--- src/components/Report/SliderIncome/SliderIncome.js
+++ src/components/Report/SliderIncome/SliderIncome.js
@@ -2,14 +2,32 @@ import { useSelector } from 'react-redux';
 import { kapustaSelectors } from '../../../redux/selectors';
 import SliderIncomeList from './SliderIncomeList';
 import SliderNotification from '../SliderNotification';
+// import Loader from 'react-loader-spinner';
 
 const SliderIncome = () => {
   const income = useSelector(kapustaSelectors.getCategoryIncome);
 
-  return income && income.length > 0 ? (
-    <SliderIncomeList income={income} />
-  ) : (
-    <SliderNotification category="доходов" />
+  return (
+    <>
+      {income && income.length > 0 ? (
+        <SliderIncomeList income={income} />
+      ) : (
+        <SliderNotification category="доходов" />
+      )}
+
+      {/* {true && (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <Loader type="ThreeDots" color="#FF751D" height={50} width={50} />
+        </div>
+      )} */}
+    </>
   );
 };
 
