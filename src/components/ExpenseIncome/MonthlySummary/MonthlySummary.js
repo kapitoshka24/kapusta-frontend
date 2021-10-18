ac478fa957cf46a0508c2900cfd26211b6c39b67
Nataliia Byshovets <natashabysh11@gmail.com>
2021-10-19 00:30:41 +0300

    minor fixes

--- src/components/ExpenseIncome/MonthlySummary/MonthlySummary.js
+++ src/components/ExpenseIncome/MonthlySummary/MonthlySummary.js
@@ -24,38 +24,6 @@ function MonthlySummary() {
             <span>{total}</span>
           </li>
         ))}
-        <li className={styles.MonthItem}>
-          <span>НОЯБРЬ</span>
-          <span>25 500.00</span>
-        </li>
-        <li className={styles.MonthItem}>
-          <span>ОКТЯБРЬ</span>
-          <span>25 000.00</span>
-        </li>
-        <li className={styles.MonthItem}>
-          <span>СЕНТЯБРЬ</span>
-          <span>25 000.00</span>
-        </li>
-        <li className={styles.MonthItem}>
-          <span>АВГУСТ</span>
-          <span>20 000.00</span>
-        </li>
-        <li className={styles.MonthItem}>
-          <span>ИЮЛЬ</span>
-          <span>20 000.00</span>
-        </li>
-        <li className={styles.MonthItem}>
-          <span>ИЮНЬ</span>
-          <span>18 000.00</span>
-        </li>
-        <li className={styles.MonthItem}>
-          <span>МАЙ</span>
-          <span>18 000.00</span>
-        </li>
-        <li className={styles.MonthItem}>
-          <span>АПРЕЛЬ</span>
-          <span>18 000.00</span>
-        </li>
       </ul>
     </div>
   );
