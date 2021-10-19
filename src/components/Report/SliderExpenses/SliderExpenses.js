ca8e119e3b7e36332a87e28ef057fadbf7999a96
Urko Movchan <xyyrma@gmail.com>
2021-10-19 16:24:38 +0300

    fix adaptive height

--- src/components/Report/SliderExpenses/SliderExpenses.js
+++ src/components/Report/SliderExpenses/SliderExpenses.js
@@ -1,19 +1,11 @@
-import { useSelector } from 'react-redux';
-import { kapustaSelectors } from '../../../redux/selectors';
 import SliderExpensesList from './SliderExpensesList';
 import SliderNotification from '../SliderNotification';
 
-const SliderExpenses = () => {
-  const expenses = useSelector(kapustaSelectors.getCategoryExpenses);
-
-  return (
-    <>
-      {expenses && expenses.length > 0 ? (
-        <SliderExpensesList expenses={expenses} />
-      ) : (
-        <SliderNotification category="расходов" />
-      )}
-    </>
+const SliderExpenses = ({ expenses }) => {
+  return expenses?.length > 0 ? (
+    <SliderExpensesList expenses={expenses} />
+  ) : (
+    <SliderNotification category="расходов" />
   );
 };
 
